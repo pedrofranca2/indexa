1ed17bd491928184eb104c1d79d0d1511f49f129
Pedro Franca <pedrom.franca@hotmail.com>
2024-01-02 16:20:40 -0300

    feat: criando separadores e estilizando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from './components/container/container.component';
 import { HeaderComponent } from './components/header/header.component';
+import { SeparatorComponent } from './components/separator/separator.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ContainerComponent, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, ContainerComponent, HeaderComponent, SeparatorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'indexa-project';
+  alphabet = 'abcdefghijklmnopqrstuvwxyz';
 }
